de56baa5e874cb6426ec62a2a700d3ffc53c8637
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-11-21 15:38:24 +0530

    Day10

---
+++ Day10/inbuiltpipe/inbuiltpipe.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inbuiltpipe',
+  templateUrl: './inbuiltpipe.component.html',
+  styleUrls: ['./inbuiltpipe.component.css']
+})
+export class InbuiltpipeComponent {
+
+    strData:string="welcome to Angular moDule by suMit Raokahnde";
+
+    mydate=new Date();
+
+  }
+
